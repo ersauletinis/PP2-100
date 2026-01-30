4e621d2bc29e406f5e7ae18cf0a57715caee3490
Ersaulet <ersauletinis@gmail.com>
2026-01-30 07:51:58 +0500

    Add Lab1 folder with all files

---
+++ numbers.py
@@ -0,0 +1,50 @@
+#There are three numeric types in Python:
+# int
+# float
+# complex
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+
+print(type(x))
+print(type(y))
+print(type(z))
+#To verify the type of any object in Python, use the type() function:
+
+# 2 Int
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# 3 Float
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# 4 also float
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# 5 Complex
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
---
+++ variables.py
@@ -0,0 +1,10 @@
+#1
+# x = 5 # this is variable saved as a integer 
+# y = "John"# and this variable saved str
+# print(x) 
+# print(y) 
+#2
+x=4
+x="sally"
+print(x)
+
